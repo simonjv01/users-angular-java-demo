c1401146333098917474bcbdd65722bc1455043c
Simon Vargas <sjvargas01@gmail.com>
2022-10-09 22:08:05 -0400

    first commit

---
+++ src/main/java/com/example/usersangularjavademo/UsersAngularJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersangularjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAngularJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersAngularJavaDemoApplication.class, args);
+    }
+
+}
